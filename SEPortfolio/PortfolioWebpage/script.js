ccea563c882310c94974cef9e39408ada0689a9f
Brianna Brockington <149329110+briannab1997@users.noreply.github.com>
2025-12-04 12:34:26 -0500

    Updated website, added new projects with updated interface.

--- SEPortfolio/PortfolioWebpage/script.js
+++ SEPortfolio/PortfolioWebpage/script.js
@@ -1,6 +1,4 @@
-function toggleMenu () {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function toggleMenu() {
+  const links = document.querySelector(".nav-links");
+  links.classList.toggle("open");
 }
